d508d433d6dfb70273e6ffc8abc60b0f079d8b09
daniel <daniel.d4@icloud.com>
2019-10-14 18:08:40 +0200

    Initial Commit

---
+++ WallP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WallP
+//
+//  Created by daniel d4 on 14/10/2019.
+//  Copyright © 2019 daniel d4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
